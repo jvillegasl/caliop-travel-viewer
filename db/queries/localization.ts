497bf1ab71e775e91676e2563ccad3f48fb616ab
jvillegasl <jvillegasl@uni.pe>
2024-03-17 12:50:29 -0500

    feat: Integrate localization with SQL Server database

--- db/queries/localization.ts
+++ db/queries/localization.ts
@@ -1,17 +1,71 @@
 import "server-only";
 
 import { TravelItem } from "@/types";
-import { sleep } from "@/utils/sleep";
-import { RETURN_LOCALIZATION, ROUTE_LOCALIZATION } from "../data/localization";
+import { Localization } from "../models";
+import { sqlConnect } from "../connect";
+import sql from "mssql";
+import { TravelType } from "@/enums/travelType";
 import { TravelCoords } from "@/types/travelCoords";
 
-export async function selLocalizationByTravel(travel: TravelItem) {
-    await sleep(Math.random() * 3000);
+const tablePrefixByTravelType = {
+    [TravelType.VISIT]: "Visit",
+    [TravelType.EXPENSE]: "VisitExpense",
+} satisfies Record<TravelType, string>;
 
-    const coords: TravelCoords = {
-        routeCoords: ROUTE_LOCALIZATION,
-        returnCoords: RETURN_LOCALIZATION,
+const idFieldByTravelType = {
+    [TravelType.VISIT]: "VisitDetailId",
+    [TravelType.EXPENSE]: "VisitId",
+} satisfies Record<TravelType, string>;
+
+export async function selLocalizationByTravel(
+    travel: TravelItem,
+): Promise<TravelCoords> {
+    await sqlConnect();
+
+    const tablePrefix = tablePrefixByTravelType[travel.type] ?? "Visit";
+
+    const idField = idFieldByTravelType[travel.type] ?? "VisitId";
+
+    const tableRoute = tablePrefix + "RouteLocalization";
+    const tableReturn = tablePrefix + "ReturnLocalization";
+
+    function getQueryString(table: string, idField: string) {
+        return `
+			SELECT
+				T.Id,
+				T.Longitude,
+				T.Latitude,
+				T.CreateDate,
+				T.InsertDate,
+				T.InsertState,
+				T.Row
+			FROM ${table} AS T
+			WHERE T.${idField} = @Id
+			ORDER BY T.Row ASC, T.InsertDate ASC;
+		`;
+    }
+
+    const routeQueryString = getQueryString(tableRoute, idField);
+
+    const returnQueryString = getQueryString(tableReturn, idField);
+
+    const queryString = `
+		${routeQueryString}
+		${returnQueryString}
+	`;
+
+    const request = new sql.Request()
+        .input("Id", sql.Int, travel.id)
+        .query<[Localization[], Localization[]]>(queryString);
+
+    const results = await request;
+    const recordSets = results.recordsets;
+
+    const routeCoords = recordSets[0].flat();
+    const returnCoords = recordSets[1].flat();
+
+    return {
+        routeCoords,
+        returnCoords,
     };
-
-    return coords;
 }
