d67e6174ecdbf6b24536d13a5886e736173caba5
jvillegasl <jvillegasl@uni.pe>
2024-03-17 12:22:34 -0500

    feat: Type env variables

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        DB_SERVER: string;
+        DB_PORT: string;
+        DB_NAME: string;
+        DB_USER: string;
+        DB_PWD: string;
+    }
+}
